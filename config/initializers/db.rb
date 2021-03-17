8645e1c043d880fe2bc970986b61d751ccc3f035
SupDex <symeton@gmail.com>
2021-03-17 11:03:19 +0300

    fix init db. Use Sequel update_on_create

--- config/initializers/db.rb
+++ config/initializers/db.rb
@@ -1,10 +1,8 @@
-puts Settings.db.to_hash
-
 Sequel.connect(Settings.db.to_hash)
 
 Sequel::Model.db.extension(:pagination)
 
 Sequel::Model.plugin :validation_helpers
-Sequel::Model.plugin :timestamps, update_or_create: true
+Sequel::Model.plugin :timestamps, update_on_create: true
 
 Sequel.default_timezone = :utc
