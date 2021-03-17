29975d64fdd6c02710158c636125e9c8e53742fa
SupDex <symeton@gmail.com>
2021-03-17 12:59:41 +0300

    add Sequel::NotNullConstraintViolation to hander errors in ApiErrors. Remove PagyService

--- app/helpers/api_errors.rb
+++ app/helpers/api_errors.rb
@@ -2,22 +2,22 @@ class Roda
   module RodaPlugins
     module ApiErrors
       module InstanceMethods
-        # Roda.plugin(:error_handler) do |e|
-        #   case e
-        #   when Sequel::NoMatchingRow
-        #     response.status = :not_found
-        #
-        #     error_response I18n.t(:not_found, scope: 'api.errors')
-        #   when Sequel::UniqueConstraintViolation
-        #     respose.status = :not_unique
-        #
-        #     error_response I18n.t(:not_unique, scope: 'api.errors')
-        #   when Sequel::NotNullConstraintViolation, Roda::RodaPlugins::Validations::InvalidParams, KeyError
-        #     response.status = 422
-        #
-        #     error_response I18n.t(:missing_parameters, scope: 'api.errors')
-        #   end
-        # end
+        Roda.plugin(:error_handler) do |e|
+          case e
+          when Sequel::NoMatchingRow
+            response.status = :not_found
+
+            error_response I18n.t(:not_found, scope: 'api.errors')
+          when Sequel::UniqueConstraintViolation
+            respose.status = :not_unique
+
+            error_response I18n.t(:not_unique, scope: 'api.errors')
+          when Sequel::NotNullConstraintViolation, Roda::RodaPlugins::Validations::InvalidParams, KeyError
+            response.status = 422
+
+            error_response I18n.t(:missing_parameters, scope: 'api.errors')
+          end
+        end
 
         private
 
