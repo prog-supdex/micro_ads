65527b82e8686905f7ef59368640cad0c4d306f7
SupDex <symeton@gmail.com>
2021-03-17 12:53:23 +0300

    remove debug from helpers/validations

--- app/helpers/validations.rb
+++ app/helpers/validations.rb
@@ -4,22 +4,17 @@ class Roda
       class InvalidParams < StandardError; end
 
       module RequestMethods
-
         def validate_with!(validation:, params:)
           result = validate_with(validation: validation, params: params)
-          puts "failure? #{result.failure?}"
+
           raise InvalidParams if result.failure?
 
-          puts 'here!!!'
           result
         end
 
         def validate_with(validation:, params:)
-          puts 'checking params'
           contract = validation.new
-          puts params.inspect
           result = contract.call(params)
-          puts result.to_h.inspect
 
           result
         end
