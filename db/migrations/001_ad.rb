0f25bf29727c04a10dcd3e755b04a73168b3ee54
SupDex <symeton@gmail.com>
2021-03-17 10:59:46 +0300

    add column city and remove null: false for lat and lon

--- db/migrations/001_ad.rb
+++ db/migrations/001_ad.rb
@@ -5,9 +5,10 @@ Sequel.migration do
 
       column :user_id, :Bignum, null: false
       column :title, 'character varying', null: false
+      column :city, 'character varying', null: false
       column :description, 'text', null: false
-      column :lat, 'double precision', null: false
-      column :lon, 'double precision', null: false
+      column :lat, 'double precision'
+      column :lon, 'double precision'
       column :created_at, 'timestamp(6) without time zone', null: false
       column :updated_at, 'timestamp(6) without time zone', null: false
 
