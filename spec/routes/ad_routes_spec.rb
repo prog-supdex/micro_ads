190d9084cee5f63e8ea6696277dffd66064290ed
SupDex <symeton@gmail.com>
2021-03-21 12:04:38 +0300

    add spec configuration and specs

---
+++ spec/routes/ad_routes_spec.rb
@@ -0,0 +1,111 @@
+RSpec.describe AdRoutes, type: :routes do
+  describe 'GET /v1/ads' do
+    let(:user_id) { 10 }
+
+    before do
+      create_list(:ad, 3, user_id: user_id)
+    end
+
+    it 'returns a collection of ads' do
+      get '/v1/ads'
+
+      expect(last_response.status).to eq(200)
+      expect(response_body['data'].size).to eq(3)
+    end
+  end
+
+  describe 'POST /v1/ads' do
+    let(:user_id) { 10 }
+    let(:auth_token) { 'auth.token' }
+    let(:auth_service) { instance_double('Auth service') }
+
+    before do
+      allow(auth_service).to receive(:auth)
+        .with(auth_token)
+        .and_return(user_id)
+
+      allow(AuthService::Client).to receive(:new).and_return(auth_service)
+
+      header 'Authorization', "Bearer #{auth_token}"
+    end
+
+    context 'missing parameters' do
+      it 'returns an error' do
+        post '/v1/ads', {}.to_json, { 'CONTENT_TYPE' => 'application/json' }
+
+        expect(last_response.status).to eq(422)
+      end
+    end
+
+    context 'invalid parameters' do
+      let(:ad_params) do
+        {
+          title: 'Ad title',
+          description: 'Ad description',
+          city: ''
+        }
+      end
+
+      it 'returns an error' do
+        post '/v1/ads', { ad: ad_params }.to_json
+
+        expect(last_response.status).to eq(422)
+        expect(response_body['errors']).to include(
+          {
+            'detail' => 'Укажите город',
+            'source' => {
+              'pointer' => '/data/attributes/city'
+            }
+          }
+        )
+      end
+    end
+
+    context 'missing user_id' do
+      let(:user_id) { nil }
+
+      let(:ad_params) do
+        {
+          title: 'Ad title',
+          description: 'Ad description',
+          city: 'City'
+        }
+      end
+
+      it 'returns an error' do
+        post '/v1/ads', { ad: ad_params }.to_json
+
+        expect(last_response.status).to eq(401)
+        expect(response_body['errors']).to include('detail' => 'Доступ к ресурсу ограничен')
+      end
+    end
+
+    context 'valid parameters' do
+      let(:ad_params) do
+        {
+          title: 'Ad title',
+          description: 'Ad description',
+          city: 'City'
+        }
+      end
+
+      let(:last_ad) { Ad.last }
+
+      it 'creates a new ad' do
+        expect { post '/v1/ads', { ad: ad_params }.to_json }
+          .to change { Ad.count }.from(0).to(1)
+
+        expect(last_response.status).to eq(201)
+      end
+
+      it 'returns an ad' do
+        post '/v1/ads', { ad: ad_params }.to_json
+
+        expect(response_body['data']).to a_hash_including(
+          'id' => last_ad.id.to_s,
+          'type' => 'ad'
+        )
+      end
+    end
+  end
+end
